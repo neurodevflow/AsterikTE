f32bfc5ee5c840e80688e4a9c15827442e949a57
Hassam Moin <hassam_moin@hotmail.co.uk>
2025-08-04 12:27:49 +0000

    Content-Security-Policy
    Brevo CDN added to allowed script

--- server/index.ts
+++ server/index.ts
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
   // Content Security Policy
   res.header('Content-Security-Policy', [
     "default-src 'self'"; script-src 'self',
-    "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: https://replit.com https://cdn.brevo.com https://js.brevo.com",
+    "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: https://replit.com https://cdn.brevo.com https://js.brevo.com <script src="https://cdn.brevo.com/js/sdk-loader.js" async></script>",
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com",
     "font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com data:",
     "img-src 'self' data: https:",
